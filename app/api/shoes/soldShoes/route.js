056d14ee23941dbcaa06369c137f314c48a78d00
bren-pun <88811049+bren-pun@users.noreply.github.com>
2024-09-30 20:09:14 +0800

    update api

--- app/api/shoes/soldShoes/route.js
+++ app/api/shoes/soldShoes/route.js
@@ -1,10 +1,23 @@
-import connectMongoDB from "@/lib/mongodb";
-import Sneaker from "@/models/shoes";
-import { NextResponse } from "next/server";
+import mongoose from "mongoose";
 
-// API route for getting all available shoes
-export async function GET() {
-  await connectMongoDB();
-  const shoes = await Sneaker.find({ availability: "SOLD" });
-  return NextResponse.json({ shoes });
-}
+let isConnected = false;
+
+const connectMongoDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error", error);
+    throw new Error("Failed to connect to MongoDB");
+  }
+};
+
+export default connectMongoDB;
